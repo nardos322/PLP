027b6055f336b3bc7213efb3a695c887e9100563
nardos322 <nahuel.rlz@gmail.com>
2025-09-09 19:30:56 -0300

    fix tp

--- tp1-base/src/Expr.hs
+++ tp1-base/src/Expr.hs
@@ -23,25 +23,65 @@ data Expr
   deriving (Show, Eq)
 
 -- recrExpr :: ... anotar el tipo ...
-recrExpr = error "COMPLETAR EJERCICIO 7"
+recrExpr :: (Float -> b) -> (Float -> Float -> b) -> (Expr -> b -> Expr -> b -> b) ->
+  (Expr -> b -> Expr -> b -> b) ->  (Expr -> b -> Expr -> b -> b) -> (Expr -> b -> Expr -> b -> b) -> Expr -> b
+recrExpr fConst fRango fSuma fResta fMult fDiv expr =
+  case expr of
+    Const x -> fConst x
+    Rango a b -> fRango a b
+    (Suma l r) -> fSuma l (rec l) r (rec r)
+    (Resta l r) -> fResta l (rec l) r (rec r)
+    (Mult l r) -> fMult l (rec l) r (rec r)
+    (Div l r) -> fDiv l (rec l) r (rec r)
+  where
+    rec = recrExpr fConst fRango fSuma fResta fMult fDiv
+
 
 -- foldExpr :: ... anotar el tipo ...
-foldExpr = error "COMPLETAR EJERCICIO 7"
+foldExpr :: (Float -> b) -> (Float -> Float -> b) -> (b -> b -> b) ->
+  (b -> b -> b) -> (b -> b -> b) -> (b -> b -> b) -> Expr -> b
+foldExpr fConst fRango fSuma fResta fMult fDiv expr =
+  case expr of
+    Const x -> fConst x
+    Rango a b -> fRango a b
+    Suma l r -> fSuma (rec l) (rec r)
+    Resta l r -> fResta (rec l) (rec r)
+    Mult l r -> fMult (rec l) (rec r)
+    Div l r -> fDiv (rec l) (rec r)
+  where
+    rec = foldExpr fConst fRango fSuma fResta fMult fDiv
 
 -- | Evaluar expresiones dado un generador de números aleatorios
 eval :: Expr -> G Float
-eval = error "COMPLETAR EJERCICIO 8"
+eval = foldExpr
+  (\x gen -> (x, gen))  -- (Const x) devuelve x
+  (\a b gen -> dameUno (a, b) gen)  -- (Rango a b) devuelve un número entre a y b
+  (combinarG2 (+))
+  (combinarG2 (-))
+  (combinarG2 (*))
+  (combinarG2 (/))
+
+combinarG2 :: (a -> b -> c) -> G a -> G b -> G c
+combinarG2 op ga gb gen0 = let (a, gen1) = ga gen0       -- correr el primero con el generador
+                               (b, gen2) = gb gen1       -- correr el segundo con el generador actualizado
+                           in (op a b, gen2)             -- combinar valores y devolver nuevo generador
+
 
 -- | @armarHistograma m n f g@ arma un histograma con @m@ casilleros
 -- a partir del resultado de tomar @n@ muestras de @f@ usando el generador @g@.
 armarHistograma :: Int -> Int -> G Float -> G Histograma
-armarHistograma m n f g = error "COMPLETAR EJERCICIO 9"
+armarHistograma m n f g = 
+  let (xs, g') = muestra f n g
+      r = rango95 xs
+      h = histograma m r xs
+  in (h, g')
+
 
 -- | @evalHistograma m n e g@ evalúa la expresión @e@ usando el generador @g@ @n@ veces
 -- devuelve un histograma con @m@ casilleros y rango calculado con @rango95@ para abarcar el 95% de confianza de los valores.
 -- @n@ debe ser mayor que 0.
 evalHistograma :: Int -> Int -> Expr -> G Histograma
-evalHistograma m n expr = error "COMPLETAR EJERCICIO 10"
+evalHistograma m n expr = armarHistograma m n (eval expr)
 
 -- Podemos armar histogramas que muestren las n evaluaciones en m casilleros.
 -- >>> evalHistograma 11 10 (Suma (Rango 1 5) (Rango 100 105)) (genNormalConSemilla 0)
@@ -53,7 +93,7 @@ evalHistograma m n expr = error "COMPLETAR EJERCICIO 10"
 -- | Mostrar las expresiones, pero evitando algunos paréntesis innecesarios.
 -- En particular queremos evitar paréntesis en sumas y productos anidados.
 mostrar :: Expr -> String
-mostrar = error "COMPLETAR EJERCICIO 11"
+mostrar = error "completar ejercicio 11"
 
 data ConstructorExpr = CEConst | CERango | CESuma | CEResta | CEMult | CEDiv
   deriving (Show, Eq)
